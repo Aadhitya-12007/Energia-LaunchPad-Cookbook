541ace633432c93647c7e219e288a9ed2d89e9f0
Aadhitya-12007 <aadhitya66041@gmail.com>
2025-09-27 14:42:57 +0530

    Update LED Blink.c

--- Tiva C Series/LED Blink.c
+++ Tiva C Series/LED Blink.c
@@ -1,1 +1,17 @@
+// Code for Tiva C Series LaunchPad (blinks the red onboard LED)
+// The RED_LED macro is the standard way to reference it (or PF_1 can be used)
 
+void setup() {
+  // Initialize the RED_LED pin (PF_1) as an output.
+  pinMode(RED_LED, OUTPUT);
+}
+
+void loop() {
+  // Turn the LED on
+  digitalWrite(RED_LED, HIGH);
+  delay(1000);
+
+  // Turn the LED off
+  digitalWrite(RED_LED, LOW);
+  delay(1000);
+}
