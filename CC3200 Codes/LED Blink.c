1f255ff3713e2a24cf2409125193dd4dc47bce17
Aadhitya-12007 <aadhitya66041@gmail.com>
2025-09-27 14:42:03 +0530

    Update LED Blink.c

--- CC3200 Codes/LED Blink.c
+++ CC3200 Codes/LED Blink.c
@@ -1,1 +1,16 @@
+// Code for CC3200 LaunchPad (blinks the red onboard LED)
 
+void setup() {
+  // Initialize the RED_LED pin (PF_1) as an output.
+  pinMode(PF_1, OUTPUT);
+}
+
+void loop() {
+  // Turn the LED on
+  digitalWrite(PF_1, HIGH);
+  delay(1000); // Wait for 1000 milliseconds
+
+  // Turn the LED off
+  digitalWrite(PF_1, LOW);
+  delay(1000); // Wait for 1000 milliseconds
+}
