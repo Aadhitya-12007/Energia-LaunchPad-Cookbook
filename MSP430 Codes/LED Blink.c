4643c74cc8d5e37fc903c93c8da6d7d8bda025b4
Aadhitya-12007 <aadhitya66041@gmail.com>
2025-09-27 14:41:27 +0530

    Update LED Blink.c

--- MSP430 Codes/LED Blink.c
+++ MSP430 Codes/LED Blink.c
@@ -1,1 +1,16 @@
+// Code for MSP430 LaunchPad (blinks the red onboard LED)
 
+void setup() {
+  // Initialize the RED_LED pin (P1_0) as an output.
+  pinMode(RED_LED, OUTPUT);
+}
+
+void loop() {
+  // Turn the LED on (HIGH is the voltage level)
+  digitalWrite(RED_LED, HIGH);
+  delay(1000); // Wait for a second
+
+  // Turn the LED off by making the voltage LOW
+  digitalWrite(RED_LED, LOW);
+  delay(1000); // Wait for a second
+}
